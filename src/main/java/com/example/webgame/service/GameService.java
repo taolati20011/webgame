44780f9545c0ed4d35fa75c0d7720454172a7bbd
dungp123 <19021246@vnu.edu.vn>
2022-10-26 23:52:36 +0700

    nothing

--- src/main/java/com/example/webgame/service/GameService.java
+++ src/main/java/com/example/webgame/service/GameService.java
@@ -3,8 +3,14 @@ package com.example.webgame.service;
 import com.example.webgame.entity.Game;
 import com.example.webgame.response.GameDetailResponse;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public interface GameService {
     public GameDetailResponse getGameDetails(Integer id);
 
     public void addGameDetails(Game game);
+
+    public List<GameDetailResponse> findAll();
 }
